995f2ad21d101fab913785989126d241bb3cb331
TrinhNamTrung <trungremtry@gmail.com>
2024-07-15 18:58:52 +0700

    Chapter7-Video 80

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import { PersistGate } from 'redux-persist/integration/react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "nprogress/nprogress.css"
 import './App.scss';
 import Layout from './Layout';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
