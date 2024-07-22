55ab02871fb584e364ef0c41a11b4faba78f3c2e
TrinhNamTrung <trungremtry@gmail.com>
2024-07-22 15:08:57 +0700

    Chapter8-Video 84

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import _, { forEach } from "lodash";
-
+import "../User/DetailQuiz.scss"
 const DetailQuiz = (props) => {
     const params = useParams();
+    const location = useLocation();
+    console.log(location)
     const quizId = params.id;
 
     useEffect(() => {
@@ -39,7 +41,32 @@ const DetailQuiz = (props) => {
     console.log(">>>> check param: ", params);
     return (
         <div className="detail-quiz-container">
-            Detailquiz
+            <div className="left-content">
+                <div className="title">
+                    <h3>{location?.state?.quizTitle}</h3>
+                </div>
+                <hr></hr>
+                <div className="q-body">
+                    <img />
+                </div>
+                <div className="q-content">
+                    <div className="question">
+                        EX1:  QUEST 1
+                    </div>
+                    <div className="answer">
+                        <div className="a-childe">A. sahjhdka</div>
+                        <div className="a-childe">B. sahjhdka</div>
+                        <div className="a-childe">C. sahjhdka</div>
+                    </div>
+                </div>
+                <div className="footer">
+                    <button className="btn btn-primary">Prev</button>
+                    <button className="btn btn-secondary">Next</button>
+                </div>
+            </div>
+            <div className="right-content">
+                count down
+            </div>
         </div>
     )
 }
